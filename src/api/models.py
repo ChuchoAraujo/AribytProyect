fcf45fcef7bc1036ff996e19474e4fb853d83e39
ChuchoAraujo <jeseazuara@gmail.com>
2022-12-16 19:53:26 +0000

    cambiar de rama para pegar base de datos

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,40 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Tabla_clasificadora(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_tabla = db.Column(db.Integer, primary_key=True)
+    horas = db.Column(db.Integer, unique=False, nullable=True)
+    fecha = db.Column(db.String, unique=False, nullable=True)
+    cajas = db.Column(db.String, unique=False, nullable=True)
+    articulo = db.Column(db.String, unique=True, nullable=True)
+    lote = db.Column(db.String, unique=True, nullable=False)
+    jaulas = db.Column(db.String, unique=True, nullable=False)
+    pedido = db.Column(db.String, unique=True, nullable=False)
+    personal = db.Column(db.String, unique=False, nullable=False)
+    problema = db.Column(db.String, unique=False, nullable=False)
+    accion = db.Column(db.String, unique=False, nullable=False)
+    tiempo = db.Column(db.Float, unique=False, nullable=False)
+    velocidad = db.Column(db.Float, unique=False, nullable=True)
+    gramos = db.Column(db.Float, unique=False, nullable=True)
+
+    def __repr__(self):
+        return f'<Tabla_clasificadora {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "id_tabla": self.id_tabla,
+            "cajas": self.cajas,
+            "articulo": self.articulo,
+            "lote": self.lote,
+            "jaulas": self.jaulas,
+            "pedido": self.pedido,
+            "personal": self.personal,
+            "problema": self.problema,
+            "accion": self.accion,
+            "tiempo": self.tiempo,
+            "velocidad": self.velocidad,
+            "gramos": self.gramos,
+        }
