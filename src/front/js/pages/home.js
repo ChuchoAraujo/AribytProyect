83b0b42217dd977ddf005eb6852c856299fd6044
ChuchoAraujo <jeseazuara@gmail.com>
2022-12-30 15:27:29 +0000

    css entrada logo ok

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import { Page_logo } from "./page_logo/page_logo";
 import "../../styles/home.css";
@@ -10,17 +10,23 @@ export const Home = () => {
   const { store, actions } = useContext(Context);
   const [logo, setLogo] = useState("");
 
+  const loadLogo = useEffect(() => {
+    setLogo("opacity")}, [])
+
+    const loadLogin = useEffect(() => {
+      const timer = setTimeout(() => {
+        console.log("This will run after 1 second!");
+      }, 1000);
+      return () => clearTimeout(timer);
+    }, []);
+  
 
 
   return (
     <div
       className={logo === "opacity" ? "opacityDone" : "opacity"}
-      onLoad={() => alert("hola")}
+      onLoad={loadLogo}
     >
-      <div>
-        <button onClick={() => setLogo("opacity")}>opacity</button>
-      </div>
-      
       <div>
         <Page_logo />
       </div>
